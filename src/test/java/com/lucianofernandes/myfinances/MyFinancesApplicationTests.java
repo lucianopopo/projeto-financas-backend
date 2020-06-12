b1bd4b7873dc4f455d71a5c6847565cf9aea8b3a
luciano.fernandes <luciano.fernandes@ntendencia.com.br>
2020-06-12 15:09:07 -0300

    feat: Commit inicial do projeto com as principais dependencias.

---
+++ src/test/java/com/lucianofernandes/myfinances/MyFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianofernandes.myfinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
